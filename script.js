d125432ce2937e8b05a472d515cb26d50ce440da
alu167974-usj <alu.167974@usj.es>
2026-05-11 21:51:26 +0200

    Fixed bug

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function integerToRoman(num) {
     { value: 40, numeral: 'XL' },
     { value: 10, numeral: 'X' },
     { value: 9, numeral: 'IX' },
-    { value: 5, numeral: 'X' },
+    { value: 5, numeral: 'V' },
     { value: 4, numeral: 'IV' },
     { value: 1, numeral: 'I' }
   ];
